34a972176996d0b0b9f926bedd7c5b590a105c89
FirdausYudha <firdausyudhasakti.work@gmail.com>
2024-12-09 15:38:17 +0700

    done

---
+++ MODUL3_FIRDAUS/src/Task.java
@@ -0,0 +1,42 @@
+import java.time.LocalDate;
+
+public class Task {
+    // TO DO: Lengkapi atribut-atribut private kelas Task
+    private LocalDate dueDate;
+    private String title;
+    private String priority;
+    private String status;
+
+    // TO DO: Buat constructor untuk kelas Task
+    public void Task(LocalDate duDate, String title, String priority, String status){
+        this.dueDate = dueDate;
+        this.title = title;
+        this.priority = priority;
+        this.status = status;
+    }
+
+    // TO DO: Buat getter untuk title
+    public String getTitle() {
+        return title;
+    }
+
+    // TO DO: Buat getter untuk priority
+    public String getPriority() {
+        return priority;
+    }
+
+    // TO DO: Buat getter untuk dueDate
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    // TO DO: Buat getter untuk status
+    public String getStatus() {
+        return status;
+    }
+
+    // TO DO: Buat setter untuk status
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
